343d5bc0b9651b429c136b6a2b83fa19a30b410d
SamathSeck <108096354+SamathSeck@users.noreply.github.com>
2024-11-27 09:22:47 +0000

    first commit

---
+++ PizzaStore/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PizzaDb>(options =>
+    options.UseInMemoryDatabase("items"));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = " API PizzaStore ",
+        Description = " Faire les pizzas que vous aimez ",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
+app.MapGet("/", () => "Bonjour Sénégal!");
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.Run();
