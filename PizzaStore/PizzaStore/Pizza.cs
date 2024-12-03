f917c969ab569c84e5b3660a12ff814b60f99b83
SamathSeck <108096354+SamathSeck@users.noreply.github.com>
2024-12-03 20:13:00 +0000

    troisième commit

---
+++ PizzaStore/PizzaStore/Pizza.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Description { get; set; }
+    }
+	public class PizzaEhod
+	{
+		public int IdEhod { get; set; }
+		public string? NomEhod { get; set; }
+		public string? DescriptionEhod { get; set; }
+	}
+
+	class PizzaDb : DbContext
+	{
+		public PizzaDb(DbContextOptions options) : base(options) { }
+		public DbSet<Pizza> Pizzas { get; set; } = null!;
+	}
+}
